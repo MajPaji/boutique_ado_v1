76e9b492f1971e0c43dd3b093953ea0e5fb0a0c5
MajPaji <sababi@kth.se>
2021-01-27 17:13:58 +0000

    started product template

--- products/views.py
+++ products/views.py
@@ -7,11 +7,16 @@ from .models import Product
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
-
     products = Product.objects.all()
 
     context = {
         'products': products
     }
+
+    user = Product.objects.get(id=1)
+
+    print(user.image.url)
+
+  
 
     return render(request, 'products/products.html', context)
