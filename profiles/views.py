0a92c009e097535be8b12814e2b1601bc84a611f
MajPaji <sababi@kth.se>
2021-02-13 19:14:41 +0000

    added order history and updated profile

--- profiles/views.py
+++ profiles/views.py
@@ -4,15 +4,17 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     """ Display the user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
-    
+
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
-            form.save()            
+            form.save()
             messages.success(request, 'Profile updated successfully')
 
     form = UserProfileForm(instance=profile)
@@ -26,3 +28,24 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+
+def order_history(request, order_number):
+
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(
+        request, (f'This is a past confirmation for order{order_number}'
+                  'A confirmation email was sent on the order date'
+                  ))
+
+    template = 'checkout/checkout_success.html'
+
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
+
+
