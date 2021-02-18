83cb9afc95320d3ed3a6a2a92e3ee5d418a9291a
MajPaji <sababi@kth.se>
2021-02-18 08:10:24 +0000

    disable collect static1

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -89,6 +89,7 @@ TEMPLATES = [
     },
 ]
 
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = (
@@ -193,7 +194,7 @@ if 'USE_AWS' in os.environ:
 
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
-    
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
